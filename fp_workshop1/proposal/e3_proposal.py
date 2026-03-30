bec89b4ee901928aa0cd95519c74496809f9b2c7
MedenAgan15 <ygagnaire@orange.fr>
2026-03-30 09:37:23 +0200

    e3_proposal imrovements (according to Loïc'suggestions)

--- fp_workshop1/proposal/e3_proposal.py
+++ fp_workshop1/proposal/e3_proposal.py
@@ -1,4 +1,5 @@
 from refs import Item
+import operator
 
 # ----------------------------------------------------------------------------------------------------
 #region ETAPE 3 - TRI
@@ -12,11 +13,19 @@ def get_category_order (item:Item) -> int:
     return int(item.category)
 
 #region Tri avc list.sort
-def sort_by_category_with_list(items:list[Item])-> list[Item]:
+def sort_by_category_with_list_deprecated(items:list[Item])-> list[Item]:
 
     result:list[Item] = list(items) # Nous créons ici un clone de l'objet liste passé en paramètre car list.sort utilisé ci-dessous modifie la liste cible
     list.sort(result, key = get_category_order) # La liste "result" est 
     return result
+
+def sort_by_category_with_list(items:list[Item])-> list[Item]:
+
+    cloned_list:list[Item] = list(items) # Nous créons ici un clone de l'objet liste passé en paramètre car list.sort utilisé ci-dessous modifie la liste cible
+    cloned_list.sort(key = operator.attrgetter('category')) 
+    print (cloned_list)
+    return cloned_list
+    
 #endregion
 
 #region Tri avec la fonction native sorted
