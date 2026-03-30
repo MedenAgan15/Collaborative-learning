1bd04c5b6b2fc30b8b63518db90baba7187580b5
MedenAgan15 <ygagnaire@orange.fr>
2026-03-30 11:43:52 +0200

    e3_proposal minor improvements

--- fp_workshop1/proposal/e3_proposal.py
+++ fp_workshop1/proposal/e3_proposal.py
@@ -16,21 +16,20 @@ def get_category_order (item:Item) -> int:
 def sort_by_category_with_list_deprecated(items:list[Item])-> list[Item]:
 
     result:list[Item] = list(items) # Nous créons ici un clone de l'objet liste passé en paramètre car list.sort utilisé ci-dessous modifie la liste cible
-    list.sort(result, key = get_category_order) # La liste "result" est 
+    list.sort(result, key = get_category_order) 
     return result
 
 def sort_by_category_with_list(items:list[Item])-> list[Item]:
 
     cloned_list:list[Item] = list(items) # Nous créons ici un clone de l'objet liste passé en paramètre car list.sort utilisé ci-dessous modifie la liste cible
     cloned_list.sort(key = operator.attrgetter('category')) 
-    print (cloned_list)
     return cloned_list
     
 #endregion
 
 #region Tri avec la fonction native sorted
 def sort_by_category_with_sorted(items:list[Item])-> list[Item]:
-    return sorted(items, key = get_category_order) # The list "items" remains unchanged and a new ordered list is returned
+    return sorted(items, key = operator.attrgetter('category')) # The list "items" remains unchanged and a new ordered list is returned
 #endregion
 
 #endregion
